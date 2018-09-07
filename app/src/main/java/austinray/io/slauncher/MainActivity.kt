b1a3620affd686d80735078fad930ecca8d42008
Austin Ray <austin@austinray.io>
2018-09-06 21:21:34 -0400

    Implement novel launcher for Chrome
    Implemented a novel application launcher that can launch Chrome. While
    basic, this was enough to learn the very basics of how an application
    launcher functions.

--- app/src/main/java/austinray/io/slauncher/MainActivity.kt
+++ app/src/main/java/austinray/io/slauncher/MainActivity.kt
@@ -2,11 +2,17 @@ package austinray.io.slauncher
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val launchIntent = packageManager.getLaunchIntentForPackage("com.android.chrome")
+            startActivity(launchIntent)
+        }
     }
 }
