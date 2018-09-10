de964a5db70a8e135c38e84be2e0746a0a4c882d
Austin Ray <austin@austinray.io>
2018-09-09 22:18:30 -0400

    Implement ViewModel for AppInfo
    Implemented a basic ViewModel to asynchronously query the package
    manager for apps.

--- app/src/main/java/io/austinray/slauncher/MainActivity.kt
+++ app/src/main/java/io/austinray/slauncher/MainActivity.kt
@@ -1,16 +1,17 @@
 package io.austinray.slauncher
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.hardware.input.InputManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
 import io.austinray.slauncher.model.AppInfo
+import io.austinray.slauncher.viewmodel.ApplicationsModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = Adapter(loadApps(), packageManager)
+        val appsModel = ViewModelProviders.of(this).get(ApplicationsModel::class.java)
+        appsModel.pm = packageManager
+
+        val adapter = Adapter(mutableListOf(), packageManager)
+
+        appsModel.apps.observeForever { apps ->
+            adapter.data = apps?.toList()!!
+            adapter.notifyDataSetChanged()
+        }
+
 
         list.layoutManager = LinearLayoutManager(applicationContext)
         list.adapter = adapter
@@ -38,21 +48,10 @@ class MainActivity : AppCompatActivity() {
             input.clearFocus()
 
             val im = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            im.hideSoftInputFromWindow(this.currentFocus.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            im.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
         }
     }
 
-    private fun loadApps() : List<AppInfo> {
-        val mainIntent = Intent(Intent.ACTION_MAIN, null)
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-        return packageManager.queryIntentActivities(mainIntent, PackageManager.GET_META_DATA)
-                .filter { it.activityInfo.loadLabel(packageManager).isNotEmpty() }
-                .map {
-                    AppInfo(it.activityInfo.packageName, it.loadLabel(packageManager).toString(),
-                            it.loadIcon(packageManager))
-                }
-                .sortedBy { it.label }
-    }
 
     override fun onResume() {
         super.onResume()
