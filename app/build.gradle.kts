5844a14bebc449c42618cda033f63df2ba3b8337
Austin Ray <austin@austinray.io>
2018-10-02 21:11:45 -0400

    Migrate dependencies to buildSrc
    buildSrc offers an easier way to manage dependencies. This method makes
    it easier to work with.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,10 @@ android {
         applicationId = Android.id
         minSdkVersion(Android.minSdk)
         targetSdkVersion(Android.targetSdk)
-        versionCode = Version.code
-        versionName = Version.name
+        versionCode = Android.code
+        versionName = Android.name
         testInstrumentationRunner = Android.instrumentationRunner
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables.useSupportLibrary = Android.supportDrawables
     }
     buildTypes {
         getByName("release") {
@@ -30,22 +28,24 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
+    implementation(Deps.kotlinStdLib)
 
-    implementation("com.android.support:appcompat-v7:28.0.0")
-    implementation("com.android.support.constraint:constraint-layout:1.1.3")
-    implementation("com.android.support:recyclerview-v7:28.0.0")
+    // Android support libraries
+    implementation(Deps.appCompat)
+    implementation(Deps.constraintLayout)
+    implementation(Deps.recyclerView)
+    implementation(Deps.supportV4)
+    implementation(Deps.vectorDrawables)
 
-    // View model libraries
-    implementation("android.arch.lifecycle:runtime:1.1.1")
-    implementation("android.arch.lifecycle:viewmodel:1.1.1")
-    implementation("android.arch.lifecycle:livedata:1.1.1")
-    implementation("android.arch.lifecycle:extensions:1.1.1")
-    implementation("com.android.support:support-v4:28.0.0")
-    implementation("com.android.support:support-vector-drawable:28.0.0")
-    kapt("android.arch.lifecycle:compiler:1.1.1")
+    // Android Lifecycle libraries
+    kapt(Deps.lifeCycleCompiler)
+    implementation(Deps.lifeCycleRuntime)
+    implementation(Deps.lifeCycleViewModel)
+    implementation(Deps.lifeCycleLiveData)
+    implementation(Deps.lifeCycleExt)
 
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("com.android.support.test:runner:1.0.2")
-    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+
+    testImplementation(Deps.jUnit)
+    androidTestImplementation(Deps.testRunner)
+    androidTestImplementation(Deps.espresso)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,17 @@ buildscript {
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.2.0")
-        classpath(kotlin("gradle-plugin", version = Versions.kotlin))
+        classpath(Deps.androidGradle)
+        classpath(Deps.kotlin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath("org.jmailen.gradle:kotlinter-gradle:1.17.0")
+        classpath(Deps.kotlinter)
     }
 }
 
 plugins {
-  id("io.gitlab.arturbosch.detekt") version("1.0.0.RC8")
+  id(Plugins.detekt) version(Vers.detekt)
 }
 
 allprojects {
@@ -32,7 +32,7 @@ task<Delete> ("clean") {
 }
 
 detekt {
-  version = "1.0.0.RC8"
+  version = Vers.detekt
     profile("Main", Action {
         input = "app/src/main/java"
         filters = ".*/resources/.*,.*/build/.*"
